d9f6aea2005dba60cd557e56f1e70fa83eead8ae
ajay <ajaygodara84557@gmail.com>
2024-10-01 15:31:00 +0530

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import './App.css'
+import React, { useState } from 'react'
 import Navbar from './components/navbar'
 
 function App() {
- 
+  let [theme, setTheme] = useState("black")
+  const changeTheme = () =>{
+      if (theme == "black"){
+          setTheme("white")
+      }
+      else{
+          setTheme("black")
+      }
+  }
   return (
-        <h1 className=" h-3screen  bg-slate-950 ">
-          <Navbar />
-        </h1>
+        <div className=" h-3screen"
+        style={{backgroundColor:theme}}>
+          <Navbar changeTheme={changeTheme} theme={theme}/>
+        </div>
       )
 }
 
