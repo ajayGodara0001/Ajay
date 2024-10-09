7cd70cf1227a1c49e28392e32e6c6753fccb6185
ajay <ajaygodara84557@gmail.com>
2024-10-09 17:42:03 +0530

    add new

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Project({pName, pInfo, tech , imgUrl, theme}) {
+function Project({pName, pInfo, tech , imgUrl, theme, liveLink, gitLink}) {
   return (
      
     <div className={` rounded-xl lg:w-[500px] lg:max-h-[500px] md:max-w-[400px] md:max-h-[500px] w-[350px] max-h-[500px] shadow-lg  ${theme === "black" ? "text-white" : "text-black"}  ${theme === "black" ? "bg-slate-900" : "shadow-gray-400"} `}>
@@ -12,14 +12,16 @@ function Project({pName, pInfo, tech , imgUrl, theme}) {
         <img className='h-32 w-full object-cover' src={imgUrl} alt="image" />
       </div>
       <div>
-        <div className='text-white text-lg bg-blue-800 m-4 text-center py-1 rounded-xl hover:bg-blue-950 hover:cursor-pointer'>Live Link</div>
-        <div className='text-white text-lg bg-blue-800 m-4 text-center py-1 rounded-xl hover:bg-blue-950 hover:cursor-pointer'>Github Link</div>
+        
+          <a target='_blank' href={liveLink}><div className='text-white text-lg bg-blue-800 m-4 text-center py-1 rounded-xl hover:bg-blue-950 hover:cursor-pointer'>Live Link</div></a>
+
+        
+        <a target='_blank' href={gitLink}><div className='text-white text-lg bg-blue-800 m-4 text-center py-1 rounded-xl hover:bg-blue-950 hover:cursor-pointer'>Github Link</div></a>
       </div>
     </div>
 
   </div>
   )
 }
-
 export default Project
 
--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Project from './project'
 
+
 function Projects({theme}) {
   return (
     <div className='w-full pt-28  flex flex-col items-center' id="project">
@@ -10,17 +11,23 @@ function Projects({theme}) {
       <div className='grid md:grid-cols-2 grid-cols-1 gap-10 items-center'>
 
       
-       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl="https://images.unsplash.com/photo-1726293993561-ae9901d8c6d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8" theme={theme}/>
+       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl={"../../public/p1.png"} theme={theme} 
+       liveLink={"https://ajaygodaraportfolio.vercel.app/"} gitLink={"https://github.com/ajayGodara0001/Ajay"}/>
       
-       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl="https://images.unsplash.com/photo-1726293993561-ae9901d8c6d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"  theme={theme}/>
+       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl={"../../public/p1.png"}  theme={theme}
+       liveLink={"https://ajaygodaraportfolio.vercel.app/"} gitLink={"https://github.com/ajayGodara0001/Ajay"}/>
       
-       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl="https://images.unsplash.com/photo-1726293993561-ae9901d8c6d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"  theme={theme}/>
+       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl={"../../public/p1.png"}  theme={theme}
+       liveLink={"https://ajaygodaraportfolio.vercel.app/"} gitLink={"https://github.com/ajayGodara0001/Ajay"}/>
       
-       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl="https://images.unsplash.com/photo-1726293993561-ae9901d8c6d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"  theme={theme}/>
+       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript"imgUrl={"../../public/p1.png"}  theme={theme}
+       liveLink={"https://ajaygodaraportfolio.vercel.app/"} gitLink={"https://github.com/ajayGodara0001/Ajay"}/>
       
-       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl="https://images.unsplash.com/photo-1726293993561-ae9901d8c6d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"  theme={theme}/>
+       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript"imgUrl={"../../public/p1.png"} theme={theme}
+       liveLink={"https://ajaygodaraportfolio.vercel.app/"} gitLink={"https://github.com/ajayGodara0001/Ajay"}/>
       
-       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl="https://images.unsplash.com/photo-1726293993561-ae9901d8c6d0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"  theme={theme}/>
+       <Project pName="Project Name" pInfo="Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, fuga. Reiciendis ullam explicabo molestias amenda dolore! " tech="Tailwind, React, JavaScript" imgUrl={"../../public/p1.png"}  theme={theme}
+       liveLink={"https://ajaygodaraportfolio.vercel.app/"} gitLink={"https://github.com/ajayGodara0001/Ajay"}/>
      
       
 
